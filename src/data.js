fc8109447d8f29e208603cd768dece012557be7d
Ignavillegas <129542098+Ignavillegas@users.noreply.github.com>
2023-07-24 10:24:41 -0300

    trabajo final frontend Jorge Ignacio Tito Villegas
    Trabajo final UTN

---
+++ src/data.js
@@ -0,0 +1,53 @@
+
+export const data = [
+
+	{
+		id: 1,
+		img: '/img/pro max celu.avif',
+		nameProduct: '📱Iphone📱',
+		price: 200.599,
+		quantity: 1,
+	},
+
+	{
+		id: 2,
+		img: '/img/mac.avif',
+		nameProduct: '💻MacBook💻',
+		price: 
+		960.899,
+		quantity: 1,
+	},
+
+	{
+		id: 3,
+		img: '/img/pc escritorio.avif',
+		nameProduct: '🖥Mac🖥',
+		price: 864.899,
+		quantity: 1,
+	},
+
+	{
+		id: 4,
+		img: '/img/cascos realidad virtual.avif',
+		nameProduct: '👓Watch👓',
+		price: 254.699,
+		quantity: 1,
+	},
+
+	{
+		id: 5,
+		img: '/img/ipad.avif',
+		nameProduct: '📱Ipad🖋',
+		price: 343.799,
+		quantity: 1,
+	},
+
+	{
+		id: 6,
+		img: "/img/conjunto-cables.avif",
+		nameProduct: '🔌USB🔌',
+		price: 38.599,
+		quantity: 1,
+	},
+
+];
